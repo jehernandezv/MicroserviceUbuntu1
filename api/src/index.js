4c53f6e132c817da75c73819cf52ef2ea2ca365c
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-07-20 20:03:21 -0500

    Se implementa la funcion de enviar un log a un determinado server por medio de axios de nodejs

--- api/src/index.js
+++ api/src/index.js
@@ -1,18 +1,48 @@
 var express = require('express');
 var cors = require('cors');
-const morgan = require('morgan');
+var morgan = require('morgan');
+var axios = require('axios');
+var body_parser = require('body-parser');
+var os = require('os');
+
 var app = express();
 var port = process.argv[2];
+var ipMidleware = process.argv[3];
+
+
+
 app.use(cors());
 app.use(morgan('dev'));
+app.use(body_parser.urlencoded({ extended: false }));
 
-app.get('/api/ubuntu1/:min/:max', function (req, res) {
-    const {min,max} = req.params;
-    res.json({
-     message: Math.floor(Math.random() * ((max - min)) + 1)
-    });
-  });
 
-  app.listen(port, function () {
-    console.log('Example app server ubuntu1 listening on port '+ port+'!');
+app.get('/api/ubuntu1/:min/:max', async function (req, res) {
+  const { min, max } = req.params;
+  const random = Math.floor(Math.random() * ((max - min)) + 1);
+  await res.json({
+    message: random
   });
+  //Peticion al middleware para almacenar el log actual
+  await axios.post('http://'+ipMidleware+':3000/saveLog', {
+    method: req.method,
+    url:req.url,
+    httpVersion:req.httpVersion,
+    statusCode:req.res.statusCode,
+    statusMessage:req.res.statusMessage,
+    nameHost:os.hostname(),
+    systemHost:os.release(),
+    dateHost:new Date(),
+    outputServerHost:random,
+    hostOS:os.platform(),
+    hostType:os.type(),
+    hostArch:os.arch()
+  }).then(response => {
+    console.log(response.data);
+  }).catch(e => {
+    console.log(e);
+  });
+});
+
+app.listen(port, function () {
+  console.log('Example app server ubuntu1 listening on port ' + port + '!');
+});
